8dba6b4eb41346992a8d0c38cdee40030d0fda2d
nakayama <aki.gkatay.2020@gmail.com>
2018-11-12 14:29:46 +0900

    add user scaffold

---
+++ spec/views/users/index.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "users/index", type: :view do
+  before(:each) do
+    assign(:users, [
+      User.create!(
+        :name => "Name",
+        :email => "Email",
+        :gender => 2,
+        :introduction => "MyText"
+      ),
+      User.create!(
+        :name => "Name",
+        :email => "Email",
+        :gender => 2,
+        :introduction => "MyText"
+      )
+    ])
+  end
+
+  it "renders a list of users" do
+    render
+    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "Email".to_s, :count => 2
+    assert_select "tr>td", :text => 2.to_s, :count => 2
+    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+  end
+end
